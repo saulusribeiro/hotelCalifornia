fd4f687bb59f25db02b6119361167e92fdafebb8
Saulus Jose Orlando Ribeiro <saulus.ribeiro@mv.com.br>
2025-02-17 15:02:21 -0300

    Ajustando a estrutura do Projeto e criando a tabela hotel_california no banco H2

--- src/main/java/br/com/hotelCalifornia/infraestructure/model/HotelCaliforniaModel.java
+++ src/main/java/br/com/hotelCalifornia/infraestructure/model/HotelCaliforniaModel.java
@@ -2,16 +2,42 @@ package br.com.hotelCalifornia.infraestructure.model;
 
 import java.util.UUID;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+
+@Entity
+@Table(name="hotel_california")
 public class HotelCaliforniaModel {
 	
+	
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Id
 	private UUID id;
-	
+
+	@Column(name="name")
 	private String name;
-	
+
+	@Column(name="local")
 	private String local;
 	
+	@Column(name="capacidade")
 	private int capacidade;
 	
+	@Column(name="cnpj")
 	private String cnpj;
 
 	public String getName() {
